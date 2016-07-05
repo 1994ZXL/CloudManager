78490a56191003a13ce91dd37a06d05de65ea8cb
1994ZXL <849457199@qq.com>
2016-07-05 14:17:54 +0800

    CloudManagersystem

---
+++ app/src/main/java/com/example/zxl/cloudmanager/LoginFragment.java
@@ -0,0 +1,30 @@
+package com.example.zxl.cloudmanager;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginFragment extends Fragment {
+    private TextView usernameTextView, passwordTextView;
+    private EditText usernameEditText, passwordEditText;
+    private Button loginButton;
+
+    @Override
+    public View onCreateView(LayoutInflater layoutInflater, ViewGroup parent, Bundle saveInstanceState) {
+        View v = layoutInflater.inflate(R.layout.login_fragment, parent, false);
+
+        usernameTextView = (TextView)v.findViewById(R.id.loginFragment_username_textview);
+        passwordTextView = (TextView)v.findViewById(R.id.loginFragment_password_textview);
+        usernameEditText = (EditText)v.findViewById(R.id.loginFragment_username_edittext);
+        passwordEditText = (EditText)v.findViewById(R.id.loginFragment_password_edittext);
+
+        loginButton = (Button)v.findViewById(R.id.loginFragment_loginbutton);
+
+        return v;
+    }
+}
---
+++ app/src/main/java/com/example/zxl/cloudmanager/MainFragment.java
@@ -0,0 +1,7 @@
+package com.example.zxl.cloudmanager;
+
+import android.app.Fragment;
+
+public class MainFragment extends Fragment {
+
+}
