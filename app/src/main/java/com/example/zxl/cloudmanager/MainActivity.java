d888e56065afeace8889a492cb6ab29747bc6dc8
1994ZXL <849457199@qq.com>
2016-07-05 17:34:13 +0800

    未完成的顶部滑动和pageview

--- app/src/main/java/com/example/zxl/cloudmanager/MainActivity.java
+++ app/src/main/java/com/example/zxl/cloudmanager/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.zxl.cloudmanager;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 
 
@@ -10,6 +12,14 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Fragment fragment;
+        FragmentManager fragmentManager = getFragmentManager();
+        fragment = fragmentManager.findFragmentById(R.id.main_fragment);
+
+        if (null == fragment) {
+            fragment = new MainFragment();
+            fragmentManager.beginTransaction().replace(R.id.main_fragment, fragment).commit();
+        }
 
     }
 }
