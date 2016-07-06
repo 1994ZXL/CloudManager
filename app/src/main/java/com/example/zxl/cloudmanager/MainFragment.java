40457a502019f2f5cab2bf63eed7c7eea3464130
ZQQ <549438533@qq.com>
2016-07-06 10:47:27 +0800

    删除多余的内容，登录界面优化

--- app/src/main/java/com/example/zxl/cloudmanager/MainFragment.java
+++ app/src/main/java/com/example/zxl/cloudmanager/MainFragment.java
@@ -8,21 +8,5 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 public class MainFragment extends Fragment {
-    private RadioGroup mRadioGroup;
-    private RadioButton mRB_Message;
-    private RadioButton mRB_Check;
-    private RadioButton mRB_Leave;
-    private RadioButton mRB_Mission;
-    private RadioButton mRB_Maintain;
-    private RadioButton mRB_Post;
-    private RadioButton mRB_Overtime;
-    private RadioButton mRB_Travel;
-    private RadioButton mRB_Memo;
-    private RadioButton mRB_Bug;
-    private RadioButton mRB_Usecase;
-    private ImageView mImageView;
-    private float mCurrentCheckedRadioLeft;  //所选中radiobutton距离左侧的距离
-    private HorizontalScrollView mHorizontalScrollView;  //上滑动条
-    private ViewPager mViewPager;
 
 }
