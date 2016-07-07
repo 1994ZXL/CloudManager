0ef4e650a6984f3bca5ed2100b868128c39c7041
1994ZXL <849457199@qq.com>
2016-07-07 11:19:39 +0800

    增加了我的备忘录点击事件

--- app/src/main/java/com/example/zxl/cloudmanager/MainFragment.java
+++ app/src/main/java/com/example/zxl/cloudmanager/MainFragment.java
@@ -1,6 +1,7 @@
 package com.example.zxl.cloudmanager;
 
 import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,13 +9,23 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 
 public class MainFragment extends Fragment {
-
+    private ImageView myMemoImage;
 
     @Override
     public View onCreateView(LayoutInflater layoutInflater, ViewGroup parent, Bundle saveInstanceState) {
         View v = layoutInflater.inflate(R.layout.main_fragment, parent, false);
 
-
+        myMemoImage = (ImageView)v.findViewById(R.id.main_fragment_my_memo_image);
+        myMemoImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment fragment = new MemoFragment();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragmentContiner, fragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
 
         return v;
     }
