ab3ca20c636e7a817a165b1a54eb85c1f16f98b1
1994ZXL <849457199@qq.com>
2016-07-07 16:51:41 +0800

    进一步完善了我的备忘录界面

--- app/src/main/java/com/example/zxl/cloudmanager/model/Memo.java
+++ app/src/main/java/com/example/zxl/cloudmanager/model/Memo.java
@@ -9,6 +9,20 @@ public class Memo {
     private String mMemoTitle;
     private Date mBeginDate;
     private Date mEndDate;
+    private String mContent;
+
+    public Memo(String mMemoTitle, String mContent) {
+        this.mMemoTitle = mMemoTitle;
+        this.mContent = mContent;
+    }
+
+    public String getmContent() {
+        return mContent;
+    }
+
+    public void setmContent(String mContent) {
+        this.mContent = mContent;
+    }
 
     public String getmMemoTitle() {
         return mMemoTitle;
